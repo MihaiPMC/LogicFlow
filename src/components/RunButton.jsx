84de71d6c9bdd98856a78376cb49fa2bd5821396
AndreiLucaa <mihaiandreiluca16@yahoo.com>
2025-04-06 04:46:16 +0300

    Update RunButton styles for improved visibility and user interaction

--- src/components/RunButton.jsx
+++ src/components/RunButton.jsx
@@ -4,7 +4,7 @@ const RunButton = ({ runCode }) => {
   return (
     <button
       onClick={runCode}
-      className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-5 rounded-2xl hover:cursor-pointer transition-colors duration-300"
+      className="bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-5 rounded-2xl hover:cursor-pointer transition-colors duration-300"
     >
       Rulează Codul
     </button>
