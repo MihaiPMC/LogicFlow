cdb5ea3ad75d019fa8792345ef4c25f6a8ccf981
daniel21 <mitracheadaniel@gmail.com>
2025-04-09 18:33:08 +0300

    Delete src/components/InstructionsTitle.jsx

--- src/components/InstructionsTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InstructionsTitle = ({ text }) => {
-  return (
-    <h3 className="text-lg font-bold mt-4">{text}</h3>
-  )
-}
-
-export default InstructionsTitle
